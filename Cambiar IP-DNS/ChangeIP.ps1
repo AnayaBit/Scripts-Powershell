427f4022ba772486aaa05595447f658ec45973e2
AnayaBit <feat1989@gmail.com>
2019-01-10 15:29:13 -0600

    version 1

---
+++ Cambiar IP-DNS/ChangeIP.ps1
@@ -0,0 +1,23 @@
+﻿#Por agregar repetir el script en caso de tener mas NICs
+#Lista las interfaces disponibles
+Get-NetAdapter
+Pause
+Clear-Host
+#Solicita la declaracion de variables
+$NIC = Read-Host -Prompt 'Network Card Prefix'
+$IP = Read-Host -Prompt 'IP Address'
+$MASK = Read-Host -Prompt 'Mask (ex 24)'
+$GW = Read-Host -Prompt 'Gateway'
+$DNS = Read-Host -Prompt 'DNS server'
+Write-Host "You input NIC is '$NIC'  IP'$IP'  Mask Prefix'$MASK' DNS Server '$DNS' Gateway '$GW' " 
+pause
+#New-NetIPAddress configura la ip,mascara y gateway
+#Set-DnsClientServerAddress configura el servidor DNS
+New-NetIPAddress -InterfaceIndex "$NIC" -IPAddress "$IP" -PrefixLength "$MASK" -DefaultGateway "$GW" | Out-Null
+pause
+Set-DnsClientServerAddress -InterfaceIndex "$NIC" -ServerAddresses "$DNS"
+#Muestra Los resultados de la Configuracion
+Get-NetIPAddress -InterfaceIndex "$NIC" | Sort-Object -Property InterfaceIndex | Format-Table
+pause
+Get-DnsClientServerAddress -InterfaceIndex "$NIC"
+pause
