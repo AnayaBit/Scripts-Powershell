620c13b49d9f8ccb0430a902336db676c5e29e28
AnayaBit <feat1989@gmail.com>
2019-01-17 10:16:27 -0600

    Prueba de puerto TCP

--- Probar Puerto TCP/Port.ps1
+++ Probar Puerto TCP/Port.ps1
@@ -1,3 +1,5 @@
+#ToDo
+#Agregar Funcionalidad para puertos UDP
 $ip= Read-Host -Prompt "Remote IP server"
 $port= Read-Host -Prompt "TCP Port"
 New-Object System.Net.Sockets.TCPClient("$ip", "$port") | Format-Table Connected -AutoSize
